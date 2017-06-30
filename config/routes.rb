e98c8d01c3e997eb4a5335facafe2ba83711b849
morgvanny <morgvanny@gmail.com>
2017-06-30 16:00:08 -0600

    routing to user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => 'devise/registrations' }
-  root 'static#index'
+
+  resources :users, :only => [:show]
 
   resources :characters
 
   resources :parties
+
+  root 'users#show'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
